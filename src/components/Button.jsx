e1eb46268c53fc8cfda991007b030b87cbf53a0b
DamianoMura <mura.damiano14@gmail.com>
2025-08-29 11:39:54 +0200

    [add] button component template

---
+++ src/components/Button.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {useState} from 'react'
+const Button = (props) => {
+
+  return (
+      <div className="col-2">
+        <button>
+          ciccio
+        </button>
+      </div>
+  )
+}
+
+export default Button
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import { useState } from 'react'
+import Button from './Button';
 const Main = (props) => {
   const languages = props.data;
-  const [language, setLanguage]= useState(1);
+  const [language, setLanguage]= useState(null);
   return (
     <main>
       <div className="container my-5">
@@ -10,16 +11,24 @@ const Main = (props) => {
           {languages.map(lang => {
             return (
               <div className="col-2" key={lang.id}>
-                <button className="btn btn-primary me-2" onClick={()=> setLanguage(lang)}>{lang.title}</button>
+                <button className={`btn me-2 ${ language != null && lang.id=== language.id ? `btn-warning`:`btn-primary`}`} onClick={()=> setLanguage(lang)}>{lang.title}</button>
               </div>
+             
 
             )
           })}
           </div>
           <div className="col-12 mt-4">
             <div className="card p-3">
-              <h2>{language.title}</h2>
-              <p>{language.description}</p>
+              {language===null 
+              ? (<h3>nessun linguaggio selezionato</h3>)
+              : (
+              <>
+                <h2>{language.title}</h2>
+                <p>{language.description}</p>
+              </>
+              )}
+              
             </div>
           </div>
         
